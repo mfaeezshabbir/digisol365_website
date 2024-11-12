db2cf0e6ba3097dcdb503e0c5ad407a26248e4f6
M zeeshan <m121zeeshan@gmail.com>
2024-11-12 16:19:29 +0500

    Upadated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState, useEffect } from "react";
 import HeroSection from "@/components/HeroSection";
 import { useTheme } from "../context/ThemeContext";
 import FloatingButton from "@/components/FloatingButton";
@@ -15,20 +16,40 @@ import Footer from "@/components/Footer";
 export default function Home() {
   const { state } = useTheme();
 
+  // State to track screen width
+  const [isLargeScreen, setIsLargeScreen] = useState(false);
+
+  useEffect(() => {
+    // Check if window is defined (to avoid SSR issues)
+    if (typeof window !== "undefined") {
+      // Update screen width state
+      setIsLargeScreen(window.innerWidth > 900);
+
+      // Add event listener to handle window resize
+      const handleResize = () => {
+        setIsLargeScreen(window.innerWidth > 900);
+      };
+
+      window.addEventListener("resize", handleResize);
+
+      // Cleanup event listener on component unmount
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []);
+
   return (
     <div
       className={`${
         state.theme === "dark" ? "dark" : ""
-      } max-h-[100vh] transition-all duration-500 ease-in-out flex  flex-col lg:row`}
+      } max-h-[100vh] transition-all duration-500 ease-in-out flex flex-col`}
     >
       <FloatingButton />
-      <div className="">
-        {/* Sidebar Section */}
-        {window.innerWidth > 900 && <Sidebar />}
-      </div>
+
+      {/* Conditionally render Sidebar based on screen size */}
+      {isLargeScreen && <Sidebar />}
 
       {/* Main Content Section */}
-      <div className="w-[100%] md:w-[92%] md:ml-[8%] bg-background dark:bg-foreground text-foreground dark:text-background transition-all duration-500 ease-in-out ">
+      <div className="w-[100%] md:w-[92%] md:ml-[8%] bg-background dark:bg-foreground text-foreground dark:text-background transition-all duration-500 ease-in-out">
         <HeroSection />
         <WhoWeAre />
         <ServicesSolutions />
